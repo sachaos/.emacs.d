e003d8b4e92ff8c2752655de0760f8cb5534124b
sachaos <sakataku7@gmail.com>
2015-05-10 22:22:11 +0900

    [add] customized C-a, and C-e

--- inits/99-keybinding.el
+++ inits/99-keybinding.el
@@ -13,3 +13,7 @@
 
 ;; popwin prefix
 (global-set-key (kbd "C-x C-p") popwin:keymap)
+
+;; custom C-a and C-e
+(global-set-key "\C-a" 'beginning-of-visual-indented-line)
+(global-set-key "\C-e" 'end-of-visual-line)
