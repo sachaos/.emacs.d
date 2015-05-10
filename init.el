92d936d031d7609488173ce344fba1a3f42b3d69
sachaos <sakataku7@gmail.com>
2015-05-10 15:29:39 +0900

    [mod] package.el

--- init.el
+++ init.el
@@ -1,3 +1,15 @@
+;;;;;;;;;;;;;;;;
+;; Package.el ;;
+;;;;;;;;;;;;;;;;
+(require 'package)
+
+; Add package-archives
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+; Initialize
+
+(package-initialize)
+
 ;;;;;;;;;;;;;;;;;;;;
 ;; init-loader.el ;;
 ;;;;;;;;;;;;;;;;;;;;
