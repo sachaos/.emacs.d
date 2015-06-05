af8f946afa3bbf90157f281568825ff3baa76192
sachaos <sakataku7@gmail.com>
2015-06-05 22:20:46 +0900

    [mod] auto install settings

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
 ;;;;;;;;;;;;;;;;
 (require 'eieio)
 (require 'package)
+(require 'cl)
 
 ; Add package-archives
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
@@ -10,6 +11,63 @@
 
 ; Initialize
 (package-initialize)
+
+(defvar installing-package-list
+  '(
+    coffee-mode
+    emmet-mode
+    haml-mode
+    git-commit-mode
+    git-rebase-mode
+    markdown-mode
+    sass-mode
+    web-mode
+    yaml-mode
+    ac-inf-ruby
+    async
+    auto-compile
+    auto-complete
+    dash
+    direx
+    epl
+    expand-region
+    findr
+    helm
+    helm-ls-git
+    helm-robe
+    inf-ruby
+    inflections
+    init-loader
+    jump
+    magit
+    packed
+    paredit
+    pkg-info
+    popup
+    popwin
+    projectile
+    rainbow-delimiters
+    rinari
+    robe
+    ruby-block
+    ruby-compilation
+    ruby-electric
+    ruby-end
+    smartparens
+    smooth-scroll
+    undo-tree
+    undohist
+    yasnippet
+    volatile-highlights
+    ))
+
+(let ((not-installed (loop for x in installing-package-list
+                            when (not (package-installed-p x))
+                            collect x)))
+  (when not-installed
+    (package-refresh-contents)
+    (dolist (pkg not-installed)
+        (package-install pkg))))
 
 ;;;;;;;;;;;;;;;;;;;;
 ;; init-loader.el ;;
