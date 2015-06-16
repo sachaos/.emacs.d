aee68aef0fe5f4d52f28d52edee0518ba638369f
sachaos <sakataku7@gmail.com>
2015-06-16 14:17:05 +0900

    [mod] align

--- inits/99-keybinding.el
+++ inits/99-keybinding.el
@@ -3,24 +3,32 @@
 ;;;;;;;;;;;;;;;;
 
 ;; backward delete by C-h
-(global-set-key "\C-h" 'backward-delete-char)
+(global-set-key (kbd "C-h")     'backward-delete-char)
 
 ;; help command
-(global-set-key "\M-?" 'help-command)
+(global-set-key (kbd "M-?")     'help-command)
 
 ;; expand region
-(global-set-key (kbd "C-o") 'er/expand-region)
-(global-set-key (kbd "C-M-o") 'er/contract-region)
+(global-set-key (kbd "C-o")     'er/expand-region)
+(global-set-key (kbd "C-M-o")   'er/contract-region)
 
 ;; popwin prefix
 (global-set-key (kbd "C-x C-p") popwin:keymap)
 
 ;; custom C-a and C-e
-(global-set-key "\C-a" 'beginning-of-visual-indented-line)
-(global-set-key "\C-e" 'end-of-visual-line)
+(global-set-key (kbd "C-a")     'beginning-of-visual-indented-line)
+(global-set-key (kbd "C-e")     'end-of-visual-line)
 
 ;; color describe
-(global-set-key (kbd "C-c c") 'describe-face-at-point)
+(global-set-key (kbd "C-c c")   'describe-face-at-point)
 
 ;; grep helm git
-(global-set-key (kbd "C-c g") 'helm-git-grep)
+(global-set-key (kbd "C-c g")   'helm-git-grep)
+
+;; helm
+(global-set-key (kbd "M-x")     'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-ls-git-ls-find-files)
+(global-set-key (kbd "C-x C-r") 'helm-recentf)
+(global-set-key (kbd "M-y")     'helm-show-kill-ring)
+(global-set-key (kbd "C-c i")   'helm-imenu)
+(global-set-key (kbd "C-x b")   'helm-buffers-list)
